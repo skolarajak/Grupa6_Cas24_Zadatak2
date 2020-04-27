b0e7aa47feb7ba75d3a5176fa818a804ebf7191c
Slobodan Todorov <skolarajak.qa@gmail.com>
2020-04-27 22:11:23 +0200

    Add project files.

---
+++ Cas24_Zadatak2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation;
+            double number1, number2;
+
+            Console.Write("Unesite operaciju (+, -, *, /) > ");
+            operation = Console.ReadLine();
+            Console.Write("Unesite prvi broj > ");
+            number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Unesite drugi broj > ");
+            number2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = SimpleCalc.Calc(operation, number1, number2);
+            Console.WriteLine("Rezultat operacije {0} {1} {2} = {3}", number1, operation, number2, result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cas24_Zadatak2/SimpleCalc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak2
+{
+    class SimpleCalc
+    {
+        public static double Calc(string operation, double num1, double num2)
+        {
+            switch(operation)
+            {
+                case "+":
+                    return Add(num1, num2);
+                case "-":
+                    return Subtract(num1, num2);
+                case "*":
+                    return Multiply(num1, num2);
+                case "/":
+                    return Divide(num1, num2);
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Subtract(double num1, double num2)
+        {
+            if (num2 > num1) {
+                return num2 - num1;
+            } else
+            {
+                return num1 - num2;
+            }
+        }
+
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            if (num2 > num1)
+            {
+                return num2 / num1;
+            }
+            else
+            {
+                return num1 / num2;
+            }
+        }
+
+    }
+}
